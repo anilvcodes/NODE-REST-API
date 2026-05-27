515c7d23d6f318d61a27037940de1f3e3fbc1bdf
Dev-Aniweb <anilkumarve51@gmail.com>
2026-05-27 23:53:22 +0530

    first commit '

---
+++ todo-api/server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+ 
+app.use(cors());
+app.use(express.json());
+
+let todos=[
+ {
+    id : 1,
+    todos:" node learning",
+    done:"true",
+
+},
+ {
+    id : 2,
+    todos:" docker learning",
+    done:"false",
+
+},
+ {
+    id : 3,
+    todos:" docker learning",
+    done:"false",
+
+},
+];
+
+app.get("/todos" ,(req, res)=>{
+    res.json(todos);
+})
+
+
+app.get("/todos/:id",(req,res)=>{
+    const todo = todos.find (t=> t.id == req.params.id );
+
+
+
+if(!todo){
+    return res.status(404).json({
+        msg:"todo not find"
+    })
+   
+    
+}
+res.json(todo);
+
+});
+
+app.post("todos/",(req,res)=>{
+
+})
+
+app.put("todos/"=>(req,res)=>{
+    
+})
+
+
+app.delete("todos/",(req,res)=>{
+
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
