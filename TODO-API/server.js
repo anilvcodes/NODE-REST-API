3b9b35ca04835f6fee4ab1fd47693d910356feff
anilvcodes <anilkumarve51@gmail.com>
2026-09-01 20:51:21 +0530

    change

--- TODO-API/server.js
+++ TODO-API/server.js
@@ -94,8 +94,8 @@ app.delete("/todos/:id", (req, res) => {
     });
 });
 
-const PORT = 3000;
 
-app.listen(PORT, () => {
+
+app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
 });
